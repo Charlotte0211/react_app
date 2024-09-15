10b69cd81e86b8ea292b6fb3b889a8012f44b9a5
Charlotte0211 <69457852+Charlotte0211@users.noreply.github.com>
2024-09-15 20:06:31 +0100

    Adding search engine

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1> Hello World</h1>
+        <h1>Weather app</h1>
         <Weather city="paris" />
       </header>
     </div>
--- src/Weather.js
+++ src/Weather.js
@@ -1,16 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
-export default function Weather(props) {
-  function handleResponse(response) {
-    alert(
-      `The weather in ${response.data.name} is ${response.data.main.temp} degree celsius `
-    );
+export default function WeatherSearch() {
+  const [city, setCity] = useState("");
+  const [loaded, setLoaded] = useState(false);
+  const [weather, setWeather] = useState(null);
+
+  function HandleSubmit(event) {
+    event.preventDefault();
+    let apiKey = "616b14cbd38253313b3b8852fa77335d";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayWeather);
   }
-  let apiKey = "616b14cbd38253313b3b8852fa77335d";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
 
-  axios.get(apiUrl).then(handleResponse);
+  function displayWeather(response) {
+    setLoaded(true);
+    setWeather({
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      icon: `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+    });
+  }
 
-  return <h2> Hello from React</h2>;
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
+  let form = (
+    <form onSubmit={HandleSubmit}>
+      <input
+        type="search"
+        placeholder="Enter a city..."
+        onChange={updateCity}
+      />
+      <button type="Submit"> Search</button>
+    </form>
+  );
+
+  if (loaded) {
+    return (
+      <div>
+        {form}
+        <ul>
+          <li>Temperature:{Math.round(weather.temperature)}</li>
+          <li>Wind: {weather.wind}Km/h</li>
+          <li>Humidity: {weather.humidity}%</li>
+          <li>
+            <img src={weather.icon} alt="Weather icon" />
+          </li>
+        </ul>
+      </div>
+    );
+  } else {
+    return form;
+  }
 }
