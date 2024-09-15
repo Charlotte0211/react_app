5b6839cc70ebf77bc972ab5689437845ce93552b
Charlotte0211 <69457852+Charlotte0211@users.noreply.github.com>
2024-09-15 20:19:41 +0100

    Adding link to gitHub and Netlify

--- src/App.js
+++ src/App.js
@@ -10,6 +10,17 @@ function App() {
         <h1>Weather app</h1>
         <Weather city="paris" />
       </header>
+      <br />
+      <footer>
+        This project was coded by Charlotte Rivière and is{" "}
+        <a href="https://github.com/Charlotte0211/react_app" target="_blank">
+          open-sourced on GitHub{" "}
+        </a>{" "}
+        and hosted on{" "}
+        <a href="https://enchanting-torte-2bd828.netlify.app/" target="_blank">
+          Netlify.
+        </a>
+      </footer>
     </div>
   );
 }
