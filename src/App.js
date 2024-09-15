43f158e54b49df29827c65056921d37650ad8260
Charlotte0211 <69457852+Charlotte0211@users.noreply.github.com>
2024-09-15 18:57:06 +0100

    Adding react
    Adding react to my project in visual code

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Weather from "./Weather";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1> Hello World</h1>
+        <Weather />
+      </header>
+    </div>
+  );
+}
+
+export default App;
